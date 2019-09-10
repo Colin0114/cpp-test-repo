b90a80cbfa510b2d246655a522c5c7279364ff91
zzh3 <zzh3@meitu.com>
2019-09-10 17:07:26 +0800

    Fix test-nullptr.cc bug

--- test-nullptr.cc
+++ test-nullptr.cc
@@ -3,40 +3,28 @@
 #ifdef NULL
 #undef NULL
 #endif
-#define NULL (void *)0
+#define NULL 0
+
 
 void Print(int){
     std::cout << "params is : int" << std::endl;
 }
 
+
 void Print(int *){
     std::cout << "params is : int *" << std::endl;
 }
 
-int* getPointer(){
-    return nullptr;
-}
-
-int getInt(){
-    return 0;
-}
 
 int main(){
     
-    Print(nullptr);
+    Print(NULL);
     
-    auto rt = getPointer();
+    int *rt = nullptr;
     if( rt == 0){
         std::cout << "rt is equal 0" << std::endl;
     }else{
         std::cout << "rt is not equal 0" << std::endl;
-    }
-
-    auto rt2 = getInt();
-    if(rt == nullptr){
-        std::cout << "rt2 is a nullptr" << std::endl;
-    }else{
-        std::cout << "rt2 is not a nullptr but a integer" << std::endl;
     }
 
     return 0;
