a83c070ddbacadf4a4291908603cc4dd3665f73a
zzh3 <zzh3@meitu.com>
2019-09-23 14:29:01 +0800

    Add test files 6

--- test-move-constructor.cc
+++ test-move-constructor.cc
@@ -3,24 +3,17 @@
 class B{
     public:
         B(int b){
-            pi_ = std::make_unique<int>(b);
+            b_ = b;
         }
 
-        B(const B& b){
-            std::cout << "B(B& b) is called" << std::endl;
-        }
-
-        ~B(){
-            std::cout << "~B(): " << *pi_ << std::endl;
-        }
-    private:
-        std::unique_ptr<int> pi_;
+        B(const B& b) = delete;
+        B(B&& b) = default; // compile failed if commented
+   private:
+    int b_;
 };
 
 int main(){
-
     B b(10);
-    B bm(std::move(b)); // will core dump
-
+    B bm( std::move(b) );
     return 0;
 }
